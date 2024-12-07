77f712d30686d7a5e1dc39e831d463b2c7aa6a75
tcklpl <negroniluan@gmail.com>
2024-12-07 12:14:46 -0300

    main arg parsing and command calling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use crate::cli_args::{Cli, Commands};
+use crate::command::init::init_repository;
+use clap::Parser;
+use colored::Colorize;
+use std::path::Path;
+
+mod cli_args;
+mod command;
+mod repository;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Init {} => init_repository(Path::new(".")),
+    }
 }
